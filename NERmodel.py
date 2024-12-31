40ef8046c8e33ae0ef262495b9fa8cd64b4bdf40
Avigail Shklovsky <9013825@gmail.com>
2024-12-31 12:27:48 +0200

    try to figure out

--- NERmodel.py
+++ NERmodel.py
@@ -1,3 +1,27 @@
+# from builtins import str
+# from transformers import AutoModel, AutoTokenizer
+
+# # Specify a cache directory to store model files on disk
+# cache_directory = "./cache"
+
+# # Load tokenizer and model with caching
+# tokenizer = AutoTokenizer.from_pretrained(
+#     'dicta-il/dictabert-joint',
+#     cache_dir=cache_directory  # Specify cache directory
+# )
+# model = AutoModel.from_pretrained(
+#     'dicta-il/dictabert-joint',
+#     cache_dir=cache_directory,  # Specify cache directory
+#     trust_remote_code=True
+# )
+
+# model.eval()
+
+# def analyze_text_with_model(sentence: str):
+#     return model.predict([sentence], tokenizer, output_style='json')
+
+
+
 from builtins import str
 from transformers import AutoModel, AutoTokenizer
 
--- app.py
+++ app.py
@@ -1,4 +1,32 @@
-from builtins import Exception, int, str
+# from builtins import Exception, int, str
+# from flask import Flask, request, jsonify
+# from flask_cors import CORS
+# import os
+
+# app = Flask(__name__)
+# CORS(app)
+
+# @app.route('/analyze', methods=['POST'])
+# def analyze_text():
+#     from NERmodel import analyze_text_with_model  # Import here to save memory
+#     data = request.json
+#     sentence = data.get('text')
+
+#     if not sentence:
+#         return jsonify({"error": "No text provided"}), 400
+
+#     try:
+#         result = analyze_text_with_model(sentence)
+#         return jsonify({"embeddings": result})
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
+# if __name__ == '__main__':
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port, debug=True)
+
+
+from builtins import Exception, int, print, str
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
@@ -8,9 +36,11 @@ CORS(app)
 
 @app.route('/analyze', methods=['POST'])
 def analyze_text():
+    print("in the py func")
     from NERmodel import analyze_text_with_model  # Import here to save memory
     data = request.json
     sentence = data.get('text')
+
 
     if not sentence:
         return jsonify({"error": "No text provided"}), 400
