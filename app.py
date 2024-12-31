9b9bdf5a6beb70fe7be72eccf607c12aeccba327
Avigail Shklovsky <9013825@gmail.com>
2024-12-31 13:15:54 +0200

    fix ?

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def analyze_text():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    # port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=8080, debug=True)
