69db10241d7206012c81d25069f676da155c80df
Avigail-Shklovsky <148010735+Avigail-Shklovsky@users.noreply.github.com>
2025-01-03 11:49:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-# from builtins import Exception, int, str
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# import os
-
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/analyze', methods=['POST'])
-# def analyze_text():
-#     from NERmodel import analyze_text_with_model  # Import here to save memory
-#     data = request.json
-#     sentence = data.get('text')
-
-#     if not sentence:
-#         return jsonify({"error": "No text provided"}), 400
-
-#     try:
-#         result = analyze_text_with_model(sentence)
-#         return jsonify({"embeddings": result})
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
-
-# if __name__ == '__main__':
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port, debug=True)
-
-
 from builtins import Exception, int, print, str
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -36,7 +8,7 @@ import re
 
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/analyze": {"origins": ["http://127.0.0.1:8080", "https://pythonflask-production.up.railway.app",re.compile(r"https://.*\.vercel\.app"),]}})
+cors = CORS(app, resources={r"/analyze": {"origins": ["http://127.0.0.1:8080","http://localhost:3000" "https://pythonflask-production.up.railway.app",re.compile(r"https://.*\.vercel\.app"),]}})
 
 
 @app.route('/analyze', methods=['POST'])
